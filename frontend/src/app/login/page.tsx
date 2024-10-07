98ba609da4cbceb6245a34650e8195a5a3ada2c8
ambiti0n <fabulousfresh010@gmail.com>
2024-10-07 08:54:01 +0100

    chore: particle connect implementation

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,23 +1,34 @@
-//
-
+"use client";
 import AuthParent from "@/components/auth/AuthParent";
-import Button from "@/components/Button";
+// import Button from "@/components/Button";
 import Link from "next/link";
 import React from "react";
+import { ConnectButton, useAccount } from "@particle-network/connectkit";
 
 function Login() {
+  const { address, isConnected, chainId } = useAccount();
   return (
     <main>
       <AuthParent>
         <div className="h-full flex flex-col w-[353px] items-center m-auto justify-center">
           <h1 className="text-4xl font-bold">Welcome 👋🏼</h1>
           <p className="mt-1">Please Login to your account</p>
-          <div className="h-[0.1px] w-full opacity-40 my-6 bg-white"></div>
+          <div className="h-[0.1px] w-full opacity-40 my-6 bg-white"> </div>
           <p className="text-center">
             You will be redirected to the sign up page if you don’t have an
             account
           </p>
-          <Button text={"Connect Wallet"} className="mt-5 w-full" />
+          <div className="mt-10">
+            {isConnected ? (
+              <>
+                <h2>Address: {address}</h2>
+                <h2>Chain ID: {chainId}</h2>
+              </>
+            ) : (
+              <ConnectButton />
+            )}
+          </div>
+          {/* <Button text={"Connect Wallet"} className="mt-5 w-full" /> */}
           <div className="flex items-center gap-1 mt-10">
             <p>Need to create an account?</p>
             <Link href={"signup"} className="font-medium">
