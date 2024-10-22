5bc9c9c6173f3086ad4358b9adf8e78d7445ec78
ambiti0n <fabulousfresh010@gmail.com>
2024-10-22 21:24:43 +0100

    chore: maquee integration

---
+++ frontend/src/components/Marque.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+const Marquee = ({ children }) => {
+  return (
+    <div className="relative flex overflow-x-hidden">
+      {/* First scroll container */}
+      <div className="animate-marquee whitespace-nowrap py-3 pr-8">
+        {Array(2)
+          .fill(children)
+          .map((content, i) => (
+            <span key={i} className="mx-4">
+              {content}
+            </span>
+          ))}
+      </div>
+
+      {/* Second scroll container for seamless loop */}
+      <div className="animate-marquee2 absolute top-0 whitespace-nowrap py-3 pr-8">
+        {Array(2)
+          .fill(children)
+          .map((content, i) => (
+            <span key={i} className="mx-4">
+              {content}
+            </span>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
